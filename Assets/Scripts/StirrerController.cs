c6a9df16edc6e1179bfc11012aae081b97813daa
1311katlee <katrina.lhc.lee@gmail.com>
2025-04-21 19:01:31 -0700

    Update StirrerController.cs

--- Assets/Scripts/StirrerController.cs
+++ Assets/Scripts/StirrerController.cs
@@ -2,15 +2,12 @@ using UnityEngine;
 
 public class StirrerController : MonoBehaviour
 {
-    public float rotationSpeed = 0f;
+    public DialController dial;
 
     void Update()
     {
-        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
-    }
-
-    public void SetSpeed(float newSpeed)
-    {
-        rotationSpeed = newSpeed;
+        float rpm = dial.currentRPM;
+        float degreesPerSecond = rpm * 6f; // 1 RPM = 6 degrees/sec
+        transform.Rotate(Vector3.up, degreesPerSecond * Time.deltaTime);
     }
 }
